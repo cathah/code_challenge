016eba30204283ef48e8e6dbb7a3a04af69a811b
cathah <catherine.ann.aherne@gmail.com>
2024-07-21 14:29:25 +0100

    Add endpoint stubs

--- client/main.py
+++ client/main.py
@@ -1,5 +1,85 @@
+import requests
+
 def main():
+
     print("Hello! This is the client!")
+
+
+    home_menu = '''
+------- Welcome to the Inventory Management System! -------
+Please select your action from the list below by entering the corresponding number:
+1. Hello World
+2. List all inventory
+3. List inventory by ID
+4. List event log
+5. Accept delivery
+6. Sell item
+7. Take stock
+8. Generate report
+'''
+
+
+    print(home_menu)
+
+    home_input = int(input())
+    selected_function = select_home_function(home_input)
+    selected_function()
+
+
+
+def select_home_function(i):
+    switcher = {
+            1: lambda: hello_world,
+            2: lambda: list_all_inventory,
+            3: lambda: list_inventory_by_id,
+            4: lambda: list_all_events,
+            5: lambda: accept_delivery,
+            6: lambda: sell_item,
+            7: lambda: take_stock,
+            8: lambda: generate_report,
+
+    }
+    func = switcher.get(i, lambda: 'Invalid Choice')
+    return func()
+
+
+
+
+
+def hello_world():
+    print('Hello World!')
+    response_object = requests.get("http://127.0.0.1:5000/")
+    print(response_object.json())
+
+def list_all_inventory():
+    response_object = requests.get("http://127.0.0.1:5000/list_inventory/all")
+    print(response_object.json())
+
+def list_inventory_by_id():
+    response_object = requests.get("http://127.0.0.1:5000/list_inventory/<ing_id>")
+    print(response_object.json())
+
+def list_all_events():
+    response_object = requests.get("http://127.0.0.1:5000/list_events/all")
+    print(response_object.json())
+
+def accept_delivery():
+    response_object = requests.get("http://127.0.0.1:5000/accept_delivery")
+    print(response_object.json())
+
+def sell_item():
+    response_object = requests.get("http://127.0.0.1:5000/sell_item")
+    print(response_object.json())
+
+def take_stock():
+    response_object = requests.get("http://127.0.0.1:5000/take_stock")
+    print(response_object.json())
+
+def generate_report():
+    response_object = requests.get("http://127.0.0.1:5000/generate_report")
+    print(response_object.json())
+
+
 
 if __name__ == "__main__":
     main()
--- server/main.py
+++ server/main.py
@@ -1,18 +1,58 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-
 def main():
-    print("Hello! This is the server!")
+    print("Hello, this is the server! To activate, please run the following command: ")
+    print("FLASK_APP=server/main flask run")
 
 
+@app.route("/")
+def index():
+    return jsonify(
+        hello = "World!!"
+    )
 
+@app.route("/list_inventory/all")
+def get_all_inventory():
+    return jsonify(
+        get = "all inventory"
+    )
 
-# Accept delivery
+@app.route("/list_inventory/<ing_id>")
+def get_inventory_by_ingredient(ing_id):
+    return jsonify(
+        get = "inventory by id"
+    )
 
-# Sell Items
+@app.route("/list_events/all")
+def get_all_events():
+    return jsonify(
+        get = "all events"
+    )
 
-# Take stock
+@app.route("/accept_delivery/")
+def accept_delivery():
+    return jsonify(
+        event = "accept delivery"
+    )
 
-# Reportingx
+@app.route("/sell_item/")
+def sell_item():
+    return jsonify(
+        event = "sell item"
+    )
+
+@app.route("/take_stock/")
+def take_stock():
+    return jsonify(
+        event = "take_stock"
+    )
+
+@app.route("/generate_report/")
+def generate_report():
+    return jsonify(
+        event = "generate_report"
+    )
+
